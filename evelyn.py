c05339bcd1c5143ee1b70795df911c5bd7d991e4
Mykayla Fernandes <my_kaykay1@yahoo.com>
2016-08-18 17:33:56 -0600

    E-0002 : add python logic placeholder file

---
+++ evelyn.py
@@ -0,0 +1,32 @@
+#logic
+
+
+class Food:
+    def Good:
+        pass
+    def Bad:
+        pass
+
+class Activities:
+    def Good:
+        pass
+    def Bad:
+        pass
+
+class People:
+    def Good:
+        pass
+    def Bad:
+        pass
+
+class Objects:
+    def Good:
+        pass
+    def Bad:
+        pass
+
+class Creatures:
+    def Good:
+        pass
+    def Bad:
+        pass
